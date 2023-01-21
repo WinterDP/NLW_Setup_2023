e676944973d2b5d0e40cf642d6c9a1e89b330527
João Prado <59054486+WinterDP@users.noreply.github.com>
2023-01-21 19:07:25 -0300

    aula 04 problema

--- script.js
+++ script.js
@@ -32,11 +32,29 @@ são informações armazenadas de diversas formas, desde textos números ou bool
 /*============== configuração inicial =========== */
 const form = document.querySelector("#form-habits") // atribui o form do HTML como uma variável
 const nlwSetup = new NLWSetup(form) // ativação da lib
+const addDayButton = document.querySelector("header button") // recebe o botão da página
+
+addDayButton.addEventListener("click", addDate) // atribui um evento ao clique do botão na página
+
+//================== Funções ==========================
+
+function addDate() {
+  const today = new Date().toLocaleDateString("pt-br").slice(0, -5) // pega a data do dia de hoje no formato dd/mm/yyyy e recorta os 5 ultimos caracteres da String
+
+  if (!nlwSetup.dayExists(today)) {
+    //verifica se a data adicionada já existe
+    alert("Adicionado com sucesso ✅")
+    nlwSetup.addDay(today)
+  } else {
+    alert("Dia já incluso ❌")
+  }
+}
 
 //utilização da biblioteca para adicionar um habito praticado em uma data
 
 //criação de um objeto data com habitos praticados em determinados dias
 
+/** 
 const data = {
   run: ["02-02", "02-04", "02-05", "02-06", "02-07", "02-08"], //array de datas no formato necessário mm-dd
   exercise: ["02-01", "02-04"],
@@ -45,3 +63,4 @@ const data = {
 
 nlwSetup.setData(data)
 nlwSetup.load()
+*/
